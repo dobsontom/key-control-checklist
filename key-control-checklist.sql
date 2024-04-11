1c45286844fc5b73f80da6bd4f3586e7c63dfaa2
Tom Dobson <dobs.tx@gmail.com>
2024-04-11 15:29:39 +0100

    Add IME01-W, alphabetise breakdown_scaffold

--- key-control-checklist.sql
+++ key-control-checklist.sql
@@ -12,20 +12,9 @@ WITH
     ),
     breakdown_scaffold AS (
         SELECT DISTINCT
-            'F12-M' AS control,
-            CAST(ErrorMessageID AS STRING) AS breakdown
-        FROM
-            revenue-assurance-prod.control_f12m_btp_suspense.tableau_summary
-        UNION DISTINCT
-        SELECT DISTINCT
-            'IME01-W' AS control,
-            ErrorMessageID AS breakdown
-        FROM
-            revenue-assurance-prod.ime_suspense.IME_Tableau_Summary
-        UNION DISTINCT
-        SELECT DISTINCT
             'A04-Q' AS control,
-            sap_exception AS breakdown
+            sap_exception AS breakdown,
+            'Control Count' AS metric
         FROM
             revenue-assurance-prod.control_a04q_rebill.alteryx_output
         WHERE
@@ -34,12 +23,47 @@ WITH
                 'SAP data not found',
                 'No SAP Exceptions'
             )
+        UNION DISTINCT
+        SELECT DISTINCT
+            'F12-M' AS control,
+            CAST(ErrorMessageID AS STRING) AS breakdown,
+            'Count of Errors' AS metric
+        FROM
+            revenue-assurance-prod.control_f12m_btp_suspense.tableau_summary
+        UNION DISTINCT
+        SELECT DISTINCT
+            'IME01-W' AS control,
+            ErrorMessageID AS breakdown,
+            'Count of Errors' AS metric
+        FROM
+            revenue-assurance-prod.ime_suspense.IME_Tableau_Summary
+        UNION DISTINCT
+        SELECT DISTINCT
+            'IME02-W' AS control,
+            CONCAT(traffic_type, ' - ', IME_AcquisitionPortal) AS breakdown,
+            metric
+        FROM
+            revenue-assurance-prod.control_ime_sv.IME_SV_Summary
+            CROSS JOIN (
+                SELECT DISTINCT
+                    metric
+                FROM
+                    revenue-assurance-prod.control_ime_sv.IME_SV_Summary UNPIVOT(
+                        control_count
+                        FOR metric IN (
+                            files_collected,
+                            IME_TotRecsRecvd,
+                            IME_v_SV_difference
+                        )
+                    )
+            )
     ),
     date_breakdown_scaffold AS (
         SELECT
             breakdown_scaffold.control,
             date_scaffold.scafdate,
-            breakdown_scaffold.breakdown
+            breakdown_scaffold.breakdown,
+            breakdown_scaffold.metric
         FROM
             date_scaffold
             CROSS JOIN breakdown_scaffold
@@ -232,6 +256,97 @@ WITH
                 WHERE
                     date_breakdown_scaffold.control = 'IME01-W'
             )
+    ),
+    ime02w_data AS (
+        SELECT
+            "IME02-W" AS `Control`,
+            scafdate AS `Date`,
+            last_refresh AS `Last Refresh`,
+            breakdown AS `Breakdown`,
+            metric AS `Metric`,
+            control_count AS `Control Count`,
+            actual_diff AS `Actual Difference vs Yesterday`,
+            pct_diff `Pct Difference vs Yesterday`
+        FROM
+            (
+                SELECT
+                    scafdate,
+                    last_refresh,
+                    breakdown,
+                    metric,
+                    control_count,
+                    LAG(control_count) OVER (
+                        PARTITION BY
+                            breakdown,
+                            metric
+                        ORDER BY
+                            scafdate
+                    ) AS errors_yesterday,
+                    control_count - LAG(control_count) OVER (
+                        PARTITION BY
+                            breakdown,
+                            metric
+                        ORDER BY
+                            scafdate
+                    ) AS actual_diff,
+                    SAFE_DIVIDE(
+                        control_count - LAG(control_count) OVER (
+                            PARTITION BY
+                                breakdown,
+                                metric
+                            ORDER BY
+                                scafdate
+                        ),
+                        LAG(control_count) OVER (
+                            PARTITION BY
+                                breakdown,
+                                metric
+                            ORDER BY
+                                scafdate
+                        )
+                    ) * 100 AS pct_diff
+                FROM
+                    (
+                        SELECT
+                            date_breakdown_scaffold.scafdate,
+                            date_breakdown_scaffold.breakdown,
+                            (
+                                SELECT
+                                    MAX(ime_ime_file_date)
+                                FROM
+                                    revenue-assurance-prod.control_ime_sv.IME_SV_Summary
+                            ) AS last_refresh,
+                            date_breakdown_scaffold.metric,
+                            IFNULL(SUM(control_count), 0) AS control_count,
+                        FROM
+                            date_breakdown_scaffold
+                            LEFT JOIN (
+                                SELECT
+                                    *
+                                FROM
+                                    revenue-assurance-prod.control_ime_sv.IME_SV_Summary UNPIVOT(
+                                        control_count
+                                        FOR metric IN (
+                                            files_collected,
+                                            IME_TotRecsRecvd,
+                                            IME_v_SV_difference
+                                        )
+                                    )
+                            ) a ON date_breakdown_scaffold.scafdate = a.ime_ime_file_date
+                            AND date_breakdown_scaffold.breakdown = CONCAT(a.traffic_type, ' - ', a.IME_AcquisitionPortal)
+                            AND date_breakdown_scaffold.metric = a.metric
+                        WHERE
+                            date_breakdown_scaffold.control = 'IME02-W'
+                        GROUP BY
+                            date_breakdown_scaffold.scafdate,
+                            date_breakdown_scaffold.breakdown,
+                            metric
+                        ORDER BY
+                            date_breakdown_scaffold.scafdate,
+                            date_breakdown_scaffold.breakdown,
+                            date_breakdown_scaffold.metric
+                    )
+            )
     )
 SELECT
     *
@@ -246,4 +361,9 @@ UNION ALL
 SELECT
     *
 FROM
-    a04q_data;
+    a04q_data
+UNION ALL
+SELECT
+    *
+FROM
+    ime02w_data;
