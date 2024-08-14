f267e280bcc9aa60e8930b844f88a989721a18f5
Tom Dobson <dobs.tx@gmail.com>
2024-08-14 11:44:36 +0100

    Add comments to show which controls use which sources

--- control_refresh_times.sql
+++ control_refresh_times.sql
@@ -1,4 +1,5 @@
 CREATE OR REPLACE VIEW `revenue-assurance-prod.key_control_checklist.control_refresh_times` AS (
+   -- Last refresh source for: A02-Q, A17-M, F01-M 
    SELECT
       table_id,
       TIMESTAMP_MILLIS(last_modified_time) AS last_refresh
@@ -7,6 +8,7 @@ CREATE OR REPLACE VIEW `revenue-assurance-prod.key_control_checklist.control_ref
    WHERE
       table_id = 'vw_project_tasks'
    UNION ALL
+   -- Last refresh source for: A04-Q
    SELECT
       table_id,
       TIMESTAMP_MILLIS(last_modified_time) AS last_refresh
@@ -15,6 +17,7 @@ CREATE OR REPLACE VIEW `revenue-assurance-prod.key_control_checklist.control_ref
    WHERE
       table_id = 'alteryx_output'
    UNION ALL
+   -- Last refresh source for: A06-M
    SELECT
       table_id,
       TIMESTAMP_MILLIS(last_modified_time) AS last_refresh
@@ -23,6 +26,7 @@ CREATE OR REPLACE VIEW `revenue-assurance-prod.key_control_checklist.control_ref
    WHERE
       table_id = 'dim_lease_history'
    UNION ALL
+   -- Last refresh source for: F12-M
    SELECT
       table_id,
       TIMESTAMP_MILLIS(last_modified_time) AS last_refresh
@@ -31,6 +35,7 @@ CREATE OR REPLACE VIEW `revenue-assurance-prod.key_control_checklist.control_ref
    WHERE
       table_id = 'sim_tracker'
    UNION ALL
+   -- Last refresh source for: IMEO1-W
    SELECT
       table_id,
       TIMESTAMP_MILLIS(last_modified_time) AS last_refresh
@@ -39,6 +44,7 @@ CREATE OR REPLACE VIEW `revenue-assurance-prod.key_control_checklist.control_ref
    WHERE
       table_id = 'EPS_Suspense'
    UNION ALL
+   -- Last refresh source for: IMEO2-W
    SELECT
       table_id,
       TIMESTAMP_MILLIS(last_modified_time) AS last_refresh
@@ -47,6 +53,7 @@ CREATE OR REPLACE VIEW `revenue-assurance-prod.key_control_checklist.control_ref
    WHERE
       table_id = 'ime_summary'
    UNION ALL
+   -- Last refresh source for: VAR-1
    SELECT
       table_id,
       TIMESTAMP_MILLIS(last_modified_time) AS last_refresh
@@ -55,6 +62,7 @@ CREATE OR REPLACE VIEW `revenue-assurance-prod.key_control_checklist.control_ref
    WHERE
       table_id = 'output_var_leases_alteryx_data'
    UNION ALL
+   -- Last refresh source for: X01-B
    SELECT
       table_id,
       TIMESTAMP_MILLIS(last_modified_time) AS last_refresh
@@ -62,4 +70,14 @@ CREATE OR REPLACE VIEW `revenue-assurance-prod.key_control_checklist.control_ref
       `revenue-assurance-prod.pulse_src.__TABLES__`
    WHERE
       table_id = 'vessel'
+   UNION ALL
+   -- Last refresh source for: GX4-JX
+   SELECT
+      table_id,
+      TIMESTAMP_MILLIS(last_modified_time) AS last_refresh
+   FROM
+      `revenue-assurance-prod.control_gx4.__TABLES__`
+   WHERE
+      table_id = 'output_control_outcomes'
+
 );
