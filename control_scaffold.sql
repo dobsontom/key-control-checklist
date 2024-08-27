f3e64f38d26a7014884e1e84029f403cb7677e37
Tom Dobson <dobs.tx@gmail.com>
2024-08-27 17:51:39 +0100

    Refactor query to improve maintainability

--- control_scaffold.sql
+++ control_scaffold.sql
@@ -14,13 +14,14 @@
  */
 CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.control_scaffold` AS (
    WITH
+      -- Create a scaffold of dates for the last 5 days
       date_scaffold AS (
          SELECT
             *
          FROM
             UNNEST (GENERATE_DATE_ARRAY(DATE_SUB(CURRENT_DATE, INTERVAL 5 DAY), DATE_SUB(CURRENT_DATE, INTERVAL 1 DAY))) AS scafdate
       ),
-      breakdown_scaffold AS (
+      a02q_scaffold AS (
          SELECT
             'A02-Q' AS control,
             'Category 1' AS scafmetric,
@@ -33,7 +34,8 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.control_sc
                SELECT
                   'Timing issue - active billing billing task - check in the next control run' AS category1
             )
-         UNION ALL
+      ),
+      a04q_scaffold AS (
          SELECT
             'A04-Q' AS control,
             'SAP Exception' AS scafmetric,
@@ -41,12 +43,13 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.control_sc
          FROM
             (
                SELECT
-                  'Exception, SAP data found but totals mismatch' AS sap_exception,
+                  'Exception, SAP data found but totals mismatch' AS sap_exception
                UNION ALL
                SELECT
                   'SAP data not found' AS sap_exception
             )
-         UNION ALL
+      ),
+      a06m_scaffold AS (
          SELECT
             'A06-M' AS control,
             metric AS scafmetric,
@@ -59,12 +62,14 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.control_sc
                SELECT
                   'Unpriced Lease' AS metric
             )
-         UNION ALL
+      ),
+      a15q_scaffold AS (
          SELECT
             'A15-Q' AS control,
             'Billing task completed on' AS scafmetric,
             'This month' AS scafbreakdown
-         UNION ALL
+      ),
+      a17m_scaffold AS (
          SELECT
             'A17-M' AS control,
             metric AS scafmetric,
@@ -77,17 +82,20 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.control_sc
                SELECT
                   'Vessel is inside committment period' AS metric
             )
-         UNION ALL
+      ),
+      chv_scaffold AS (
          SELECT
             'CH-V' AS control,
             'check_for_Charterer_plan_billied' AS scafmetric,
             'Review for charges - Not found in billing' AS scafbreakdown
-         UNION ALL
+      ),
+      e05w_scaffold AS (
          SELECT
             'E05-W' AS control,
             'Review Required' AS scafmetric,
             'Review' AS scafbreakdown
-         UNION ALL
+      ),
+      f01m_scaffold AS (
          SELECT
             'F01-M' AS control,
             'Tasks Remaining' AS scafmetric,
@@ -103,31 +111,36 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.control_sc
                SELECT
                   'Upgrade installation' AS project_type
             )
-         UNION ALL
+      ),
+      f12m_scaffold AS (
          SELECT DISTINCT
             'F12-M' AS control,
             'ErrorMessageID' AS scafmetric,
             CAST(errormessageid AS STRING) AS scafbreakdown
          FROM
             `revenue-assurance-prod.control_f12m_btp_suspense.tableau_summary`
-         UNION ALL
+      ),
+      fc01q_scaffold AS (
          SELECT
             'FC01-Q' AS control,
             'pulse_vs_nuda_category_1' AS scafmetric,
             'Review needed - no charges matching with Vessel ID' AS scafbreakdown
-         UNION ALL
+      ),
+      gx4jx_scaffold AS (
          SELECT
             'GX4-JX' AS control,
             'DAL vs BTP Usage by SSPC' AS scafmetric,
             'Difference Greater than 2.5%' AS scafbreakdown
-         UNION ALL
+      ),
+      ime01w_scaffold AS (
          SELECT DISTINCT
             'IME01-W' AS control,
             'ErrorMessageID' AS scafmetric,
             errormessageid AS scafbreakdown
          FROM
             `revenue-assurance-prod.ime_suspense.IME_Tableau_Summary`
-         UNION ALL
+      ),
+      ime02w_scaffold AS (
          SELECT DISTINCT
             'IME02-W' AS control,
             metric AS scafmetric,
@@ -143,7 +156,8 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.control_sc
                      FOR metric IN (files_collected, ime_totrecsrecvd, ime_v_sv_difference)
                   )
             )
-         UNION ALL
+      ),
+      var1_scaffold AS (
          SELECT
             'VAR-1' AS control,
             metric AS scafmetric,
@@ -158,7 +172,8 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.control_sc
                   'Billed_in_SV_category' AS metric,
                   'HP - billed in last 3 months' AS breakdown
             )
-         UNION ALL
+      ),
+      x01b_scaffold AS (
          SELECT DISTINCT
             'X01-B' AS control,
             'Category 1' AS scafmetric,
@@ -171,7 +186,86 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.control_sc
                SELECT
                   'Review - why billed with suspended charges although they are reactivated' AS breakdown
             )
+      ),
+      -- Union individual control scaffolds
+      breakdown_scaffold AS (
+         SELECT
+            *
+         FROM
+            a02q_scaffold
+         UNION ALL
+         SELECT
+            *
+         FROM
+            a04q_scaffold
+         UNION ALL
+         SELECT
+            *
+         FROM
+            a06m_scaffold
+         UNION ALL
+         SELECT
+            *
+         FROM
+            a15q_scaffold
+         UNION ALL
+         SELECT
+            *
+         FROM
+            a17m_scaffold
+         UNION ALL
+         SELECT
+            *
+         FROM
+            chv_scaffold
+         UNION ALL
+         SELECT
+            *
+         FROM
+            e05w_scaffold
+         UNION ALL
+         SELECT
+            *
+         FROM
+            f01m_scaffold
+         UNION ALL
+         SELECT
+            *
+         FROM
+            f12m_scaffold
+         UNION ALL
+         SELECT
+            *
+         FROM
+            fc01q_scaffold
+         UNION ALL
+         SELECT
+            *
+         FROM
+            gx4jx_scaffold
+         UNION ALL
+         SELECT
+            *
+         FROM
+            ime01w_scaffold
+         UNION ALL
+         SELECT
+            *
+         FROM
+            ime02w_scaffold
+         UNION ALL
+         SELECT
+            *
+         FROM
+            var1_scaffold
+         UNION ALL
+         SELECT
+            *
+         FROM
+            x01b_scaffold
       )
+      -- Cross join date and breakdown scaffolds to create the
+      -- final control scaffold
    SELECT
       b.control,
       d.scafdate,
