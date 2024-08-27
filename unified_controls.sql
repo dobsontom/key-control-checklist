65d2eb605c468f99651676a77fe30c455da1ed94
Tom Dobson <dobs.tx@gmail.com>
2024-08-28 00:25:24 +0100

    style: change control_scaffold alias from scf to csf

--- unified_controls.sql
+++ unified_controls.sql
@@ -25,53 +25,53 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.unified_co
       ),
       a02q_data AS (
          SELECT
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail,
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail,
             COUNTIF(
                a02q.category1 IN ('Review for charges', 'Timing issue - active billing billing task - check in the next control run')
             ) AS control_count
          FROM
-            control_scaffold scf
-            LEFT JOIN `revenue-assurance-prod.control_a02_fx_completeness.output_fx_completeness_snb_control_monthly_data` a02q ON scf.date = a02q.current_commissioning_confirmed_date
-            AND scf.metric_detail = a02q.category1
+            control_scaffold csf
+            LEFT JOIN `revenue-assurance-prod.control_a02_fx_completeness.output_fx_completeness_snb_control_monthly_data` a02q ON csf.date = a02q.current_commissioning_confirmed_date
+            AND csf.metric_detail = a02q.category1
          WHERE
-            scf.control = 'A02-Q'
+            csf.control = 'A02-Q'
          GROUP BY
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail
       ),
       a04q_data AS (
          SELECT
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail,
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail,
             COUNTIF(a04q.sap_exception IN ('Exception, SAP data found but totals mismatch', 'SAP data not found')) AS control_count
          FROM
-            control_scaffold scf
-            LEFT JOIN `revenue-assurance-prod.control_a04q_rebill.alteryx_output` a04q ON scf.date = CAST(a04q.crc_created_on AS DATE)
-            AND scf.metric_detail = a04q.sap_exception
+            control_scaffold csf
+            LEFT JOIN `revenue-assurance-prod.control_a04q_rebill.alteryx_output` a04q ON csf.date = CAST(a04q.crc_created_on AS DATE)
+            AND csf.metric_detail = a04q.sap_exception
          WHERE
-            scf.control = 'A04-Q'
+            csf.control = 'A04-Q'
          GROUP BY
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail
       ),
       a06m_data AS (
          SELECT
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail,
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail,
             COUNTIF(a06m.metric IN ('Not Billed as Planned', 'Unpriced Lease')) AS control_count
          FROM
-            control_scaffold scf
+            control_scaffold csf
             LEFT JOIN (
                -- Union two metrics into a single field
                SELECT
@@ -97,43 +97,43 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.unified_co
                      contract_value IS NULL
                      OR contract_value = 0
                   )
-            ) a06m ON scf.date = CAST(a06m.contract_start_date AS DATE)
-            AND scf.metric = a06m.metric
+            ) a06m ON csf.date = CAST(a06m.contract_start_date AS DATE)
+            AND csf.metric = a06m.metric
          WHERE
-            scf.control = 'A06-M'
+            csf.control = 'A06-M'
          GROUP BY
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail
       ),
       a15q_data AS (
          SELECT
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail,
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail,
             COUNTIF(DATE_TRUNC(a15q.billing_task_completed_on, MONTH) = DATE_TRUNC(CURRENT_DATE(), MONTH)) AS control_count
          FROM
-            control_scaffold scf
-            LEFT JOIN `revenue-assurance-prod.key_control_checklist.a15q_extract` a15q ON scf.date = DATE(a15q.billing_task_completed_on)
+            control_scaffold csf
+            LEFT JOIN `revenue-assurance-prod.key_control_checklist.a15q_extract` a15q ON csf.date = DATE(a15q.billing_task_completed_on)
          WHERE
-            scf.control = 'A15-Q'
+            csf.control = 'A15-Q'
          GROUP BY
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail
       ),
       a17m_data AS (
          SELECT
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail,
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail,
             COUNTIF(a17m.metric IN ('Null SAP Net Value', 'Vessel is inside committment period')) AS control_count
          FROM
-            control_scaffold scf
+            control_scaffold csf
             LEFT JOIN (
                -- Union two metrics into a single field
                SELECT
@@ -151,58 +151,58 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.unified_co
                   `revenue-assurance-prod.control_a17_m_fx_retail_early_terminations_fees.ETF_control_pulse_and_sdp_fees_calculated`
                WHERE
                   is_vessel_ooc = 'Vessel is inside committment period'
-            ) a17m ON scf.date = CAST(a17m.billing_task_completed_on AS DATE)
-            AND scf.metric = a17m.metric
+            ) a17m ON csf.date = CAST(a17m.billing_task_completed_on AS DATE)
+            AND csf.metric = a17m.metric
          WHERE
-            scf.control = 'A17-M'
+            csf.control = 'A17-M'
          GROUP BY
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail
       ),
       chv_data AS (
          SELECT
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail,
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail,
             COUNTIF(chv.check_for_charterer_plan_billied = 'Review for charges - Not found in billing') AS control_count
          FROM
-            control_scaffold scf
-            LEFT JOIN `revenue-assurance-prod.key_control_checklist.chv_extract` chv ON scf.date = DATE(chv.charterer_start_date)
+            control_scaffold csf
+            LEFT JOIN `revenue-assurance-prod.key_control_checklist.chv_extract` chv ON csf.date = DATE(chv.charterer_start_date)
          WHERE
-            scf.control = 'CH-V'
+            csf.control = 'CH-V'
          GROUP BY
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail
       ),
       e05w_data AS (
          SELECT
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail,
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail,
             COUNTIF(e05w.review_required = 'Review') AS control_count
          FROM
-            control_scaffold scf
-            LEFT JOIN `revenue-assurance-prod.control_e05w.output_data` e05w ON scf.date = DATE(e05w.activation_date)
+            control_scaffold csf
+            LEFT JOIN `revenue-assurance-prod.control_e05w.output_data` e05w ON csf.date = DATE(e05w.activation_date)
          WHERE
-            scf.control = 'E05-W'
+            csf.control = 'E05-W'
          GROUP BY
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail
       ),
       f01m_data AS (
          SELECT
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail,
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail,
             COUNT(
                DISTINCT IF(
                   f01m.billing_status IN ('No billing available - needs review', 'Old billing - needs review')
@@ -213,95 +213,95 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.unified_co
                )
             ) AS control_count
          FROM
-            control_scaffold scf
-            LEFT JOIN `revenue-assurance-prod.control_f01_m_pulse_projects_reconciliation.control_monthly_data` f01m ON scf.date = f01m.project_implementation_confirmed_date
-            AND scf.metric_detail = CAST(f01m.project_type AS STRING)
+            control_scaffold csf
+            LEFT JOIN `revenue-assurance-prod.control_f01_m_pulse_projects_reconciliation.control_monthly_data` f01m ON csf.date = f01m.project_implementation_confirmed_date
+            AND csf.metric_detail = CAST(f01m.project_type AS STRING)
          WHERE
-            scf.control = 'F01-M'
+            csf.control = 'F01-M'
          GROUP BY
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail
       ),
       f12m_data AS (
          SELECT
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail,
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail,
             IFNULL(f12m.countoferrors, 0) AS control_count,
          FROM
-            control_scaffold scf
-            LEFT JOIN `revenue-assurance-prod.control_f12m_btp_suspense.tableau_summary` f12m ON scf.date = f12m.chargestartdate
-            AND scf.metric_detail = CAST(f12m.errormessageid AS STRING)
+            control_scaffold csf
+            LEFT JOIN `revenue-assurance-prod.control_f12m_btp_suspense.tableau_summary` f12m ON csf.date = f12m.chargestartdate
+            AND csf.metric_detail = CAST(f12m.errormessageid AS STRING)
          WHERE
-            scf.control = 'F12-M'
+            csf.control = 'F12-M'
       ),
       fc01q_data AS (
          SELECT
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail,
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail,
             COUNTIF(fc01q.pulse_vs_nuda_category_1 = 'Review needed - no charges matching with Vessel ID') AS control_count
          FROM
-            control_scaffold scf
-            LEFT JOIN `revenue-assurance-prod.key_control_checklist.fc01q_extract` fc01q ON scf.date = DATE(fc01q.commissioning_confirm_date)
+            control_scaffold csf
+            LEFT JOIN `revenue-assurance-prod.key_control_checklist.fc01q_extract` fc01q ON csf.date = DATE(fc01q.commissioning_confirm_date)
          WHERE
-            scf.control = 'FC01-Q'
+            csf.control = 'FC01-Q'
          GROUP BY
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail
       ),
       gx4_data AS (
          SELECT
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail,
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail,
             COUNTIF(
                gx4.control_group = 'Usage'
                AND gx4.control_name = 'DAL vs BTP Usage by SSPC'
                AND gx4.exception_type = 'Difference greater than 2.5%'
             ) AS control_count
          FROM
-            control_scaffold scf
-            LEFT JOIN `revenue-assurance-prod.control_gx4.output_control_outcomes` gx4 ON scf.date = DATE(gx4.control_date)
+            control_scaffold csf
+            LEFT JOIN `revenue-assurance-prod.control_gx4.output_control_outcomes` gx4 ON csf.date = DATE(gx4.control_date)
          WHERE
-            scf.control = 'GX4-JX'
+            csf.control = 'GX4-JX'
          GROUP BY
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail
       ),
       ime01w_data AS (
          SELECT
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail,
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail,
             IFNULL(ime01w.countoferrors, 0) AS control_count
          FROM
-            control_scaffold scf
-            LEFT JOIN `revenue-assurance-prod.ime_suspense.IME_Tableau_Summary` ime01w ON scf.date = ime01w.chargestartdate
-            AND scf.metric_detail = ime01w.errormessageid
+            control_scaffold csf
+            LEFT JOIN `revenue-assurance-prod.ime_suspense.IME_Tableau_Summary` ime01w ON csf.date = ime01w.chargestartdate
+            AND csf.metric_detail = ime01w.errormessageid
          WHERE
-            scf.control = 'IME01-W'
+            csf.control = 'IME01-W'
       ),
       ime02w_data AS (
          SELECT
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail,
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail,
             -- If the count of incidents is null due to being missing from the data replace with zero
             IFNULL(SUM(ime02w.control_count), 0) AS control_count
          FROM
-            control_scaffold scf
+            control_scaffold csf
             -- Unpivot three metrics into a single field
             LEFT JOIN (
                SELECT
@@ -315,23 +315,23 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.unified_co
                      control_count
                      FOR metric IN (files_collected, ime_totrecsrecvd, ime_v_sv_difference)
                   )
-            ) ime02w ON scf.date = ime02w.ime_ime_file_date
-            AND scf.metric_detail = CONCAT(ime02w.traffic_type, ' - ', ime02w.ime_acquisitionportal)
-            AND scf.metric = ime02w.metric
+            ) ime02w ON csf.date = ime02w.ime_ime_file_date
+            AND csf.metric_detail = CONCAT(ime02w.traffic_type, ' - ', ime02w.ime_acquisitionportal)
+            AND csf.metric = ime02w.metric
          WHERE
-            scf.control = 'IME02-W'
+            csf.control = 'IME02-W'
          GROUP BY
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail
       ),
       var1_data AS (
          SELECT
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail,
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail,
             COUNTIF(
                (
                   var1.metric = 'category_1'
@@ -343,7 +343,7 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.unified_co
                )
             ) AS control_count
          FROM
-            control_scaffold scf
+            control_scaffold csf
             LEFT JOIN (
                -- Unpivot multiple metric/metric_details into a single field
                SELECT
@@ -364,23 +364,23 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.unified_co
                      metric = 'Billed_in_SV_category'
                      AND metric_detail = 'HP - billed in last 3 months'
                   )
-            ) var1 ON scf.date = CAST(var1.order_date AS DATE)
-            AND scf.metric = var1.metric
-            AND scf.metric_detail = var1.metric_detail
+            ) var1 ON csf.date = CAST(var1.order_date AS DATE)
+            AND csf.metric = var1.metric
+            AND csf.metric_detail = var1.metric_detail
          WHERE
-            scf.control = 'VAR-1'
+            csf.control = 'VAR-1'
          GROUP BY
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail
       ),
       x01b_data AS (
          SELECT
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail,
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail,
             COUNTIF(
                x01b.category1 IN (
                   'Review - active temp stop vessel, why billed with original charges',
@@ -388,16 +388,16 @@ CREATE OR REPLACE TABLE `revenue-assurance-prod.key_control_checklist.unified_co
                )
             ) AS control_count
          FROM
-            control_scaffold scf
-            LEFT JOIN `revenue-assurance-prod.control_x01b_retail_fx_temprarary_stopped_vessels_review.control_output_data_temp_stop_vessels` x01b ON scf.date = x01b.stopped_confirmed_date
-            AND scf.metric_detail = CAST(x01b.category1 AS STRING)
+            control_scaffold csf
+            LEFT JOIN `revenue-assurance-prod.control_x01b_retail_fx_temprarary_stopped_vessels_review.control_output_data_temp_stop_vessels` x01b ON csf.date = x01b.stopped_confirmed_date
+            AND csf.metric_detail = CAST(x01b.category1 AS STRING)
          WHERE
-            scf.control = 'X01-B'
+            csf.control = 'X01-B'
          GROUP BY
-            scf.control,
-            scf.date,
-            scf.metric,
-            scf.metric_detail
+            csf.control,
+            csf.date,
+            csf.metric,
+            csf.metric_detail
       ),
       combined_data AS (
          SELECT
